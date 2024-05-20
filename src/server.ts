f713ca9fa8f34c86d6e0a160757384b720e2e2a2
Sakriya Khadka <Khadkasakriya81@gamil.com>
2024-05-20 15:49:19 +0545

    origin added

--- src/server.ts
+++ src/server.ts
@@ -33,7 +33,7 @@ const allowedOrigins = config.app.allowedOrigins.split(",");
 const server = Express();
 const httpServer = createServer(server);
 server.use(cors({
-  origin:["https://citycouncil-frontend.vercel.app/","http://localhost:3000","https://master--kathmanducitycouncil.netlify.app/"],
+  origin:"https://master--kathmanducitycouncil.netlify.app/",
   methods: ["GET","POST"],
   credentials:true
 }));
